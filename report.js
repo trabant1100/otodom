c5ac9930647dabc3681f47aac88f42f4aa03cf91
trabant1100 <komelanczuk@live.com>
2024-09-29 00:40:34 +0200

    relisted

--- report.js
+++ report.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs/promises');
+const fn = require('./chart.js');
 const { DateTime } = require('luxon');
 const pug = require('pug');
 const DATE_FORMAT = 'dd.MM.yyyy';
@@ -7,9 +8,10 @@ const today = process.argv[2] ?? DateTime.now().toFormat(DATE_FORMAT);
 (async function main() {
 	const config = JSON.parse(await fs.readFile('config.json'));
 	const listingDir = config.listing.dir;
-	const { dir: reportDir, banned_urls: bannedUrls, fav_urls: favUrls, dead_urls: deadUrls } = config.report;
+	const { dir: reportDir, banned_urls: bannedUrls, fav_urls: favUrls, dead_urls: deadUrls, 
+		relisted_urls: relistedUrls } = config.report;
 
-	const report = await generateReport(today, listingDir);
+	const report = await generateReport(today, relistedUrls, listingDir);
 	console.log('Writing report json');
 	await fs.mkdir(reportDir, { recursive: true });
 	await fs.writeFile(`${reportDir}/${today}.json`, JSON.stringify(report, null, 2));
@@ -66,7 +68,7 @@ function normalize(min, max, newMin, newMax, value) {
 	return (value - min) * newDiff / diff + newMin;
 }
 
-async function generateReport(today, rootDir) {
+async function generateReport(today, relistedUrls, rootDir) {
 	const listingDirs = [];
 	for (const filename of await fs.readdir(rootDir)) {
 		const fullFilename = `${rootDir}/${filename}`;
@@ -102,6 +104,38 @@ async function generateReport(today, rootDir) {
 		}
 	}
 
+	const relistedAuctions = [];
+	for (const [auctionId, auction] of Object.entries(report)) {
+		const lastSnapshot = auction.snapshots.at(-1);
+		const url = lastSnapshot.url;
+		for (const [i, urls] of Object.entries(relistedUrls)) {
+			if (urls.includes(url)) {
+				const auction = getAuctionByUrl(url);
+				if (relistedAuctions[i] === undefined) {
+					relistedAuctions[i] = [];
+				}
+				relistedAuctions[i].push(auction);
+			}
+		}
+	}
+
+	for (const auctions of relistedAuctions) {
+		auctions.sort((auction1, auction2) => {
+			const date1 = DateTime.fromFormat(auction1.snapshots.at(-1).snapshotDate, DATE_FORMAT);
+			const date2 = DateTime.fromFormat(auction2.snapshots.at(-1).snapshotDate, DATE_FORMAT);
+			
+			return date1.toMillis() - date2.toMillis();
+		});
+		const allUrls = auctions.map(auction => auction.snapshots.at(-1).url);
+		for (const auction of auctions) {
+			for (const snapshot of auction.snapshots) {
+				snapshot.alternateUrls = allUrls;
+			}
+		}
+		const lastAuction = auctions.at(-1);
+		report[lastAuction.snapshots.at(-1).id].snapshots = auctions.flatMap(auction => auction.snapshots);
+	}
+
 	for (const auctionId in report) {
 		const auction = report[auctionId];
 		const snapshots = auction.snapshots;
@@ -110,145 +144,23 @@ async function generateReport(today, rootDir) {
 		}
 	}
 
+	function getAuctionByUrl(url) {
+		return Object.values(report).find(auction => auction.snapshots.at(-1).url == url);
+	}
+
 	return report;
 }
 
 async function createHtml(report, listingDir, { bannedUrls, favUrls, deadUrls }) {
-	const pugger = pug.compile(await fs.readFile('report.pug'));
-	const fn = {
-		parseDate(str) {
-			return DateTime.fromFormat(str, DATE_FORMAT);
-		},
-		formatMoney(price) {
-			return (new Intl.NumberFormat('pl-PL').format(price)) + ' zł'
-		},
-		calculatePriceInfos(chronosPoints, chronos, scale) {
-			const priceInfos = [];
-			let index = 0;
-			while (index < chronosPoints.length - 1) {
-				const point = chronosPoints[index];
-				let priceChangeIndex = chronosPoints.slice(index)
-					.findIndex(p => p.y != point.y);
-				priceChangeIndex = priceChangeIndex != -1 ? index + priceChangeIndex : chronosPoints.length;
-				const chartPoints = [chronosPoints[index]]
-					.concat(chronosPoints[priceChangeIndex - 1], chronosPoints[priceChangeIndex])
-					.filter(p => p != undefined);
-				const points = chartPoints
-					.concat({ x: chartPoints.at(-1).x, y: scale.price }, { x: point.x, y: scale.price });
-
-				priceInfos.push({ index, priceChangeIndex, point, points });
-				index = priceChangeIndex;
-			}
-
-			for (const [i, priceInfo] of priceInfos.entries()) {
-				const { index, points, point } = priceInfo;
-				const price = this.formatMoney(chronos[index].price);
-				const priceWidth = price.length * 3.67 + 3;
-				const trans = this.translatePriceInfo(
-					{ x: 0, y: 0, width: scale.date, height: scale.price },
-					// [points],
-					priceInfos.slice(i).map(pi => pi.points),
-					point.x, point.y, priceWidth, 10);
-				const dateRange = [
-					Math.min(...points.map(p => p.x)),
-					Math.max(...points.map(p => p.x))];
-
-				priceInfo.priceWidth = priceWidth;
-				priceInfo.price = price;
-				priceInfo.trans = trans;
-			}
-
-			return priceInfos;
-		},
-		translatePriceInfo(viewport, occupieds, x, y, width, height) {
-			const vp = dims({ left: viewport.x + 2, top: viewport.y, 
-				right: viewport.x + viewport.width - 2, bottom: viewport.y + viewport.height });
-			const ocs = occupieds.map(occupied => dims({ 
-				left: Math.min(...occupied.map(p => p.x)),
-				top: Math.min(...occupied.map(p => p.y)),
-				right: Math.max(...occupied.map(p => p.x)),
-				bottom: Math.max(...occupied.map(p => p.y)),
-				topFirst: occupied[0].y,
-			}));
-			const oc = ocs[0];
-			const it = dims({ left: x, top: y, 
-				right: x + width, bottom: y + height });
-			
-			let tx = 0;
-			let ty = 0;
-
-			if (translate(it, { ty: -12 }).top >= vp.top) {
-				ty = -12;
-				tx = 2;
-				if (it.right > vp.right) {
-					tx = -(it.width - oc.width + 2);
-				}
-			} else {
-				ty = vp.top - it.top;
-				if (translate(it, { tx: oc.width }).right <= vp.right) {
-					tx = oc.width;
-				} else {
-					tx = 2;
-				}
-			}
-
-			for (let i = ocs.length - 1; i > 0; i--) {
-				const oc = ocs[i];
-				const prevOc = ocs[i-1];
-				if (translate(it, { tx, ty }).overlaps(oc)) {
-					tx -= prevOc.width + 2;
-					if (translate(it, { tx, ty }).left < vp.left) {
-						tx = 0;
-						if (translate(it, { tx, ty}).overlaps(oc)) {
-							ty = -12 - (translate(it, { tx, ty}).bottom - oc.topFirst + 2);
-						}
-						break
-					}
-				}
-			}
-
-			function overlaps(a, b) {
-				function range(low, high) {
-					return {
-						contains: num => low <= num && num <= high,
-					};
-				}
-
-				function over(a, b) {
-					const xRange = range(a.left, a.right);
-					const yRange = range(a.top, a.bottom);
-					const ox = xRange.contains(b.left) || xRange.contains(b.right);
-					const oy = yRange.contains(b.top) || yRange.contains(b.bottom);
-					return ox && oy;
-				}
-
-				return over(a, b) || over(b, a);
-			}
-
-			function dims(item) {
-				return { 
-					...item,
-					width: item.right - item.left,
-					height: item.bottom - item.top,
-					overlaps(item2) { return overlaps(this, item2); },
-				};
-			}
-
-			function translate({ left, top, right, bottom , ...rest}, { tx = 0, ty = 0 }) {
-				return {
-					left: left + tx,
-					top: top + ty,
-					right: right + tx,
-					bottom: bottom + ty,
-					...rest,
-				};
-			}
-
-			return { x: tx, y: ty };
-		}
+	const pugger = pug.compile(await fs.readFile('report.pug'), { filename: 'pug' });
+	const scale = {
+		xMargin: 2,
+		priceMargin: 8,
+		price: 40,
+		date: 150
 	};
 
-	return pugger( { report, bannedUrls, favUrls, deadUrls, fn } );
+	return pugger( { report, bannedUrls, favUrls, deadUrls, scale, fn } );
 }
 
 function createRedirectHtml(reportDir, today) {
